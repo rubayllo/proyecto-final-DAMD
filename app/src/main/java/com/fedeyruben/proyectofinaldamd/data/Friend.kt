790322728e75fbc06eea960eeaf092edc665ec67
rubenayllonmartinez <r.ayllon@hotmail.es>
2024-05-24 12:16:16 +0200

    Solucionado scroll vista FriendScreen

--- app/src/main/java/com/fedeyruben/proyectofinaldamd/data/Friend.kt
+++
@@ -1,9 +0,0 @@
-package com.fedeyruben.proyectofinaldamd.data
-
-import android.net.Uri
-data class Friend(
-    val id: Int,
-    val nombre: String,
-    val apellido: String,
-    val imageResId: Uri? = null // Opcional: ID del recurso de imagen personalizado
-)
