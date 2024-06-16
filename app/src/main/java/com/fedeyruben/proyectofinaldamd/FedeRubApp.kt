2ee96ca8fff2fed5596ab7521e2071172f891f57
FedeRuiz <federicomaruiz@gmail.com>
2024-06-16 13:24:39 +0200

    logo

--- app/src/main/java/com/fedeyruben/proyectofinaldamd/FedeRubApp.kt
+++ app/src/main/java/com/fedeyruben/proyectofinaldamd/FedeRubApp.kt
@@ -1,7 +1,10 @@
 package com.fedeyruben.proyectofinaldamd
 
 import android.app.Application
+import androidx.room.Room
+import com.fedeyruben.proyectofinaldamd.data.room.UserDataBase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FedeRubApp: Application() {}
+class FedeRubApp: Application() {
+}
